83701b53975552827232cef74f16eb1789720bf3
Malcolm Roalson <mtroalson@gmail.com>
2024-07-30 12:57:50 -0500

    initial commit

---
+++ src/main/java/io/github/maliciousfiles/maliUtils/MaliUtils.java
@@ -0,0 +1,27 @@
+package io.github.maliciousfiles.maliUtils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MaliUtils extends JavaPlugin {
+
+    /* TODO
+        - vanish
+        - health link
+        - swap players
+        - beepers
+        - invsee
+        - smite
+        - demon mobs
+     */
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
